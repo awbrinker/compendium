b084dca7a249a97bcdbd5f863196050dbbc57db9
Alec Brinker <awbrinker@gmail.com>
2023-01-13 10:20:40 -0500

    Initial styling

---
+++ src/test/groovy/compendium/DiceControllerSpec.groovy
@@ -0,0 +1,18 @@
+package compendium
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class DiceControllerSpec extends Specification implements ControllerUnitTest<DiceController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
